0974e5c4300b12e6de83cc676459aefa2c821e1b
Persupakorn <38271471+Persupakorn@users.noreply.github.com>
2023-04-03 15:06:27 +0700

    add service life time

---
+++ Infra/ServiceLifetimes/SingletonService.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Interfaces.Infra.ServiceLifetimes;
+
+namespace Infra.ServiceLifetimes
+{
+    public class SingletonService : ISingletonService
+    {
+        private int _requestedCount;
+
+        public SingletonService() { }
+
+        public int GetRequestedCount()
+        {
+            _requestedCount = _requestedCount + 1;
+            return _requestedCount;
+        }
+    }
+}
---
+++ Infra/ServiceLifetimes/TransientService.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Interfaces.Infra.ServiceLifetimes;
+
+namespace Infra.ServiceLifetimes
+{
+    public class TransientService : ITransientService
+    {
+        private int _requestedCount;
+        private readonly IScopedService _scopeService;
+        private readonly ISingletonService _singletonService;
+
+        public TransientService(IScopedService scopeService, ISingletonService singletonService)
+        {
+            this._scopeService = scopeService;
+            this._singletonService = singletonService;
+        }
+
+        public int GetRequestedCount()
+        {
+            _requestedCount = _requestedCount + 1;
+            return _requestedCount;
+        }
+
+        public int GetScopedRequestedCount() => this._scopeService.GetRequestedCount();
+
+        public int GetSingletonRequestedCount() => this._singletonService.GetRequestedCount();
+    }
+}
